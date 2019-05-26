8b8d1d9e46f68b79bd97a9ebefda365ff180bb3e
Isuru Fernando <isuruf@gmail.com>
2019-05-25 23:48:23 -0500

    check for gregset_t

--- runtime/flangrti/aarch64-Linux/dumpregs.c
+++ runtime/flangrti/aarch64-Linux/dumpregs.c
@@ -15,17 +15,32 @@
  *
  */
 
+#include "flangrti_config.h"
+#if defined(HAVE_GREGSET_T)
 #include <sys/ucontext.h>
 
 void
 dumpregs(gregset_t *regs)
 {
 }
-  
 
 gregset_t *
 getRegs(ucontext_t *u)
 { 
   return (gregset_t *)0;
-} 
+}
 
+#else
+
+void
+dumpregs(void *regs)
+{
+}
+
+void *
+getRegs(void *u)
+{
+  return (void *)0;
+}
+
+#endif
